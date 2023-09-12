91e8246b4961e7111d1572b66b4d785fbc9308af
emrekoc <koceemree@gmail.com>
2023-09-12 09:07:42 +0300

    categorysectionupdated

--- src/test/java/pages/HomePageSearcbox.java
+++ src/test/java/pages/HomePageSearcbox.java
@@ -1,19 +1,27 @@
 package pages;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
+import java.util.List;
+
 public class HomePageSearcbox {
-    public HomePageSearcbox(){
-        PageFactory.initElements(Driver.getDriver(),this);
+    public HomePageSearcbox() {
+        PageFactory.initElements(Driver.getDriver(), this);
     }
 
-
-    @FindBy(xpath = "//span[@class='a-size-base a-color-base apb-browse-refinements-indent-1 a-text-bold']")
+    @FindAll({
+            @FindBy(xpath = "//span[@class='a-size-base a-color-base apb-browse-refinements-indent-1 a-text-bold']"),
+            @FindBy(xpath = "//h1[text()='Moda']"),
+            @FindBy(xpath = "//span[@class='a-size-medium-plus a-color-base a-text-bold']")
+    })
     public WebElement isCategoryName;
 
+
+    public WebElement isCategoryNameModa;
     @FindBy(xpath = "//input[@id='nav-search-submit-button']")
     public WebElement searchiconButton;
 }
